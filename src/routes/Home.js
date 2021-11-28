4d3901ef67ce679c3f78ac23557bd93d7c1a11a2
lhjwork <jin.come.up.business@gmail.com>
2021-11-28 21:50:29 +0900

    Dispatch se

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
 
-const Home = (props) => {
-  console.log(props);
+const Home = ({ toDos, addToDo }) => {
+  console.log(addToDo);
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    // addToDo(text);
+    addToDo(text);
     setText("");
   }
-  {
-    console.log(text);
-  }
+
   return (
     <div>
       <h1>To Do</h1>
@@ -21,14 +22,19 @@ const Home = (props) => {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul>{props.toDos}</ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </div>
   );
 };
 
-function getCurrentState(state) {
-  console.log("state", state);
+function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(getCurrentState)(Home);
+//props를 바꿀 수 있는 파워가 생김
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
