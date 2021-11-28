9e3680b00daadffee660fa8195676923a7184bb2
lhjwork <jin.come.up.business@gmail.com>
2021-11-28 19:48:59 +0900

    NoMadCode fororr react-redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./App.css";
-import Subscribers from "./components/Subscribers";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Subscribers />
-      </div>
-    </Provider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/:id" element={<Detail />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
