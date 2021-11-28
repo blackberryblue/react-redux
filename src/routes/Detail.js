284260612787c9b3ae55f28be42ebdcfc8ef0dc2
lhjwork <jin.come.up.business@gmail.com>
2021-11-28 23:05:06 +0900

    conconst id =useParams();

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { useParams } from "react-router-dom";
 
-const Detail = () => {
-  return (
-    <div>
-      <h1>Detail</h1>
-    </div>
-  );
+const Detail = (props) => {
+  const id = useParams();
+  console.log(id);
+  console.log(props);
+  return <h1>Detail</h1>;
 };
+
+// function mapStateToProps(state, ownProps) {
+//   const {
+//     match: {
+//       params: { id },
+//     },
+//   } = ownProps;
+//   return { toDo: state.find((toDo) => toDo.id === parseInt(id)) };
+// }
 
 export default Detail;
